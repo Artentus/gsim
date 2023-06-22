19993a1f85a64b85b41037f49b9e33787db5d2a5
Mathis Rech <mathisrech@gmx.de>
2023-06-22 05:01:45 +0200

    Add example to crate level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,38 @@
 //! High speed digital logic simulation
+//!
+//! ### Example
+//! ```
+//! use gsim::*;
+//!
+//! let mut builder = SimulatorBuilder::default();
+//!
+//! // Add wires and components to the simulation
+//! let wire_width = LogicWidth::new(1).unwrap();
+//! let input_a = builder.add_wire(wire_width);
+//! let input_b = builder.add_wire(wire_width);
+//! let output = builder.add_wire(wire_width);
+//! // The gate ID is not usefull to us because we don't intend on reading its data
+//! let _gate = builder.add_and_gate(input_a, input_b, output).unwrap();
+//!
+//! // Create the simulation
+//! let mut sim = builder.build();
+//!
+//! // Manually drive the input wires
+//! sim.set_wire_base_drive(input_a, LogicState::from_bool(true));
+//! sim.set_wire_base_drive(input_b, LogicState::from_bool(false));
+//!
+//! // Run the simulation
+//! const MAX_STEPS: u64 = 2;
+//! match sim.run_sim(MAX_STEPS) {
+//!     SimulationRunResult::Ok => {}
+//!     SimulationRunResult::MaxStepsReached => panic!("simulation did not settle within allowed steps"),
+//!     SimulationRunResult::Err(err) => panic!("simulation error: {err:?}"),
+//! }
+//!
+//! // Make sure we got the expected result
+//! let output_state = sim.get_wire_state(output);
+//! assert!(output_state.eq(LogicState::from_bool(false), wire_width));
+//! ```
 
 #![deny(missing_docs)]
 #![warn(missing_debug_implementations)]
@@ -353,6 +387,8 @@ type WireWidthList = wire_id::IdList<LogicWidth>;
 type WireStateList = wire_id::IdList<LogicStateCell>;
 
 /// A digital circuit simulator
+///
+/// See crate level documentation for a usage example
 #[allow(missing_debug_implementations)]
 pub struct Simulator {
     wires: WireList,
@@ -657,6 +693,8 @@ macro_rules! def_add_wide_gate {
 }
 
 /// Builds a simulator
+///
+/// See crate level documentation for a usage example
 #[allow(missing_debug_implementations)]
 #[repr(transparent)]
 pub struct SimulatorBuilder {
